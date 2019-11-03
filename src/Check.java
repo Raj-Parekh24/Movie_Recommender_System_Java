e11783bbd71ab5ee437bae03a86176177f64c062
Harshil Panchal <pharshil1902@gmail.com>
2019-11-03 14:44:31 +0530

    Menu Driven & Flow Added

--- src/Check.java
+++ src/Check.java
@@ -5,5 +5,21 @@ import java.util.Scanner;
 
 public class Check {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+      Scanner sc=new Scanner(System.in);
+
+      boolean chk=true;
+      do{
+          System.out.println("Do You Want to Sign Up or Login: 1.Sign Up 2.Login 3.QUIT");
+          int x=sc.nextInt();
+          switch(x){
+              case 1: SignUp sp=new SignUp();
+                        sp.detailsToFile();
+                        break;
+              case 2: Login lg=new Login();
+                        break;
+              case 3: chk=false;
+          }
+      }while(chk);
+
 }
 }
